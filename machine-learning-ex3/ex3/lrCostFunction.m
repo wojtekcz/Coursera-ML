fc55449313850428cec9a58049f017e72c17107a
Wojciech Czarnowski <wcz@jatar.com.pl>
2017-01-27 12:24:27 -0800

    machine-learning-ex3 completed

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -1,14 +1,14 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -25,28 +25,33 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+% compute J - vectorized
+h_th2 = sigmoid(X * theta);
+cost = -y .* log(h_th2) - (1-y) .* log(1 - h_th2);
 
+% with regularization - vectorized
+sumTheta2 = sum(theta(2:end).^2);
+J = (1/m) * sum(cost) + lambda/(2*m) * sumTheta2;
 
+% compute grad - vectorized
+B = h_th2 - y;
+grad = (1/m) * (X' * B);
 
-
-
-
-
-
+% with regularization - vectorized
+n = length(grad);
+grad(2:n) += (lambda/m) * theta(2:n);
 
 % =============================================================
-
-grad = grad(:);
 
 end
